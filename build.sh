569b1f927de4029ec0e18cbe379c8a2050284f03
duhaochi <57779438+duhaochi@users.noreply.github.com>
2021-03-05 09:44:01 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,11 @@
 
 # dowload angularLCI
 
+
+
+#git ignored the angular folder solution
+git rm --cached . -rf
+
+#angular with github
+https://www.freakyjolly.com/setup-git-in-angular-project-commit-changes/#.YEJA52hKiUk
+
