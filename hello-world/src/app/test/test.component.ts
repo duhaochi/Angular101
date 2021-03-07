45b392030a1ebe05a56291a8eb29c3ad96ad2b7e
haochi <0404haochi@gmail.com>
2021-03-07 18:46:11 -0500

    Interpolation notes and implementation in test.component.ts

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -2,14 +2,38 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: '.app-test',
-  templateUrl: './test.component.html',
+  //templateUrl: './test.component.html',
+  template: `
+  <h2>
+  <!--
+    use {{_var_}} for getting the variable
+  -->
+    Welcom test app {{name}}
+  </h2>
+
+  
+  <!--
+    calling class function
+  -->
+    <h2>{{say_hello()}}</h2>
+
+    <h3>{{"the URL of this page is: " + URL}}</h3>
+
+  `,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
 
+  //declare a dynamic string variable
+  public name = "Dan Li"
+  private names = "Hui & Du"
   constructor() { }
-
+  public URL = window.location.href
   ngOnInit(): void {
+  }
+
+  say_hello(){
+    return "Hello " + this.name
   }
 
 }
